b6fa74ab3e27651c614afccc853a1e31de72e8c1
Jatin Foujdar <jatin321001@gmail.com>
2024-11-16 21:27:40 +0530

    sign in commit

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import User from './model/userModel';
-import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs"; 
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 dotenv.config();
 
@@ -13,6 +13,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('<h1>Server has Started</h1>');
 });
+
 
 app.post("/signup", async (req, res) => {
   try {
@@ -31,6 +32,36 @@ app.post("/signup", async (req, res) => {
     const newUser = await User.create({ name, email, password: hashedPassword });
 
     res.status(201).send("User created successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
+app.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!(email && password)) {
+      res.status(400).send("Email and password are required");
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(401).send("Invalid email or password");
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      res.status(401).send("Invalid email or password");
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+      expiresIn: '1h',
+    });
+
+    res.status(200).send({ token, userId: user._id, name: user.name, email: user.email });
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
