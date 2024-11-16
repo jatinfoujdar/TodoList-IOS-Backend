520b0af46da052d689d3ea61c28ab0373b6c2565
Jatin Foujdar <jatin321001@gmail.com>
2024-11-16 21:25:09 +0530

    hashed password commit

--- server/app.js
+++ server/app.js
@@ -1,33 +1,40 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import User from './model/userModel';
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs"; 
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.json()); 
 
 app.get('/', (req, res) => {
   res.send('<h1>Server has Started</h1>');
 });
 
-app.post("/signup",async(req,res)=>{
+app.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-    const {name,email,password} = req.body
-
-    if(!(email && password && name)){
-      res.status(400).send("All the things are required")
+    if (!(email && password && name)) {
+      res.status(400).send("All fields are required");
     }
 
-    const existingUser = await User.findOne(email)
-    if(existingUser){
-        res.status(400).send("User Already Exit")
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      res.status(400).send("User already exists");
     }
-    
-    
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({ name, email, password: hashedPassword });
 
-})
-
+    res.status(201).send("User created successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 export default app;
