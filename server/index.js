6b5249463248dfc7d0fec9e48bcb1217b06329a9
Jatin Foujdar <jatin321001@gmail.com>
2024-11-16 18:28:03 +0530

    port commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import app from "./app.js"
 
-const { PORT = 4000 } = process.env;
+const { PORT } = process.env;
 
 
-app.listen(4000,()=> "Server is Running at 4000")
+app.listen(PORT,()=> "Server is Running at 4000")
