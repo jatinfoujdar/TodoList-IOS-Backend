b86c0b49b6f601043b5c55328f41a8e72310d087
Jatin Foujdar <jatin321001@gmail.com>
2024-11-16 19:15:29 +0530

    user schema commit

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -1,26 +1,27 @@
 import mongoose from "mongoose";
 
-
 const userSchema = new mongoose.Schema({
   name: {
-    type: String,  
-    required: true,  
+    type: String,
+    required: true,
+    trim: true,
   },
   email: {
     type: String,
-    unique: true,  
-    required: true  
+    unique: true,
+    required: true,
+    match: [/^\S+@\S+\.\S+$/, 'Invalid email address'],
   },
   password: {
     type: String,
+    required: true,
+    minlength: 8,
   },
   token: {
-    type: String,  
+    type: String,
   },
 });
 
-
-const User = mongoose.model("User", userSchema);  
-
+const User = mongoose.model("User", userSchema);
 
 export default User;
